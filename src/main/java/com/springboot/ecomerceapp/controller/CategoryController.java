cb02b881536f803f16d4ab1014a73e108aa6b994
imtiyaz-hirani <i.hirani@techskillsit.com>
2023-11-28 12:42:32 +0530

    cors mapping added on controllers

--- src/main/java/com/springboot/ecomerceapp/controller/CategoryController.java
+++ src/main/java/com/springboot/ecomerceapp/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.springboot.ecomerceapp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import com.springboot.ecomerceapp.service.CategoryService;
 
 @RestController
 @RequestMapping("/category")
+@CrossOrigin(origins = {"localhost:3000"})
 public class CategoryController {
 
 	@Autowired
